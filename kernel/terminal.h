c73ab117ba9bec529d6ba4a640de64f8a91ceffc
Jaedon Jacobi <jaedon@jacobi.io>
2015-03-20 16:17:45 -0700

    Slight cleanup and more work on the terminal.

--- kernel/terminal.h
+++ kernel/terminal.h
@@ -5,14 +5,14 @@
 #include <stdbool.h>
 #include "commands.h"
 
+#define PROMPT_STRING			"|>"
+#define PROMPT_START_X			0
+#define PROMPT_START_Y			14
+#define TERMINAL_INPUT_SIZE		256
 #define TERMINAL_FG				LIGHT_GREEN
 #define TERMINAL_BG				BLACK
-#define TERMINAL_INPUT_SIZE		256
 
 #define CURSOR_CHAR 			219
-#define CURSOR_START_X			1
-#define CURSOR_START_Y			14
-
 #define CURSOR_FG				LIGHT_GREEN
 #define CURSOR_BG				DARK_GREY
 
--- lib/hardware.h
+++ lib/hardware.h
@@ -2,6 +2,17 @@
 #ifndef LIB_SERIAL
 #define LIB_SERIAL
 
+//
+//	Check if a certain bit is toggled.
+//
+inline bool check_bit(int var, int pos)
+{
+	return ((var) & (1<<(pos)));
+}
+
+//
+//	Read a byte from the specified port.
+//
 inline char inb(int port)
 {
 	char ret;
@@ -9,6 +20,9 @@ inline char inb(int port)
 	return ret;
 }
 
+//
+//	Write a byte to the specified port.
+//
 inline void outb(int port, char val)
 {
 	asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));	
